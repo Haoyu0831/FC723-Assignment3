7c41c1625568bea9be40345dda976c2e3d379f29
Haoyu0831 <148050210+Haoyu0831@users.noreply.github.com>
2024-04-14 14:59:59 +0100

    Update 2.py

--- FC723 Assignment3/2.py
+++ FC723 Assignment3/2.py
@@ -1,7 +1,13 @@
 class EuclideanAlgorithm:
+
     def euclidean_algorithm(self, a, b):
+        # the function takes two integers, a and b, and find their greatest common divisor
+
+        # while loop continues as long as b is not zero
         while b != 0:
             a, b = b, a % b
+
+        # when b is zero, the current value is the greatest commmon divisor of a and b
         return a
     
 
